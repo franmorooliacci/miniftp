fa2ae1ee006b8dcd4ea9cd5842a1c12546bc73d1
franmorooliacci <franmorooliacci@gmail.com>
2026-05-10 10:27:09 -0300

    integrate logger into Makefile and initialize in main

--- iterative/main.c
+++ iterative/main.c
@@ -2,6 +2,7 @@
 #include "server.h"
 #include "utils.h"
 #include "signals.h"
+#include "logger.h"
 #include <stdio.h>
 #include <stdlib.h>     // EXIT_*
 #include <string.h>
@@ -15,11 +16,16 @@ int main(int argc, char **argv) {
   if (parse_arguments(argc, argv, &args) != 0)
     return EXIT_FAILURE;
 
-  printf("Starting server on %s:%d\n", args.address, args.port);
+  // Initialize the syslog connection immediately after parsing args
+  init_logger();
+
+  LOG_INF("Starting server on %s:%d", args.address, args.port);
 
   int listen_fd = server_init(args.address, args.port);
-  if (listen_fd < 0)
+  if (listen_fd < 0) {
+    close_logger();
     return EXIT_FAILURE;
+  }
 
   setup_signals();
 
@@ -31,15 +37,17 @@ int main(int argc, char **argv) {
 
     char client_ip[INET_ADDRSTRLEN];
     inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, sizeof(client_ip));
-    printf("Connection from %s:%d accepted\n", client_ip, ntohs(client_addr.sin_port));
+    
+    LOG_INF("Connection from %s:%d accepted", client_ip, ntohs(client_addr.sin_port));
 
     server_loop(new_socket);
 
-    printf("Connection from %s:%d closed\n", client_ip, ntohs(client_addr.sin_port));
+    LOG_INF("Connection from %s:%d closed", client_ip, ntohs(client_addr.sin_port));
   }
 
   // NEVER GO HERE
   close_fd(listen_fd, "listening socket");
+  close_logger();
 
   // https://en.cppreference.com/w/c/program/EXIT_status
   return EXIT_SUCCESS;
